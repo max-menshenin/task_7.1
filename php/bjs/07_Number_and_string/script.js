f1f3310baf41e3c0a4d56afe557f3e2a40f21f56
Max Menshenin <max.menshenin.dev@yandex.ru>
2021-12-27 13:27:57 +0300

    init

---
+++ php/bjs/07_Number_and_string/script.js
@@ -0,0 +1,24 @@
+let lastOperand = 0;
+let operation = null;
+
+const inputWindow = document.getElementById('inputWindow');
+
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+//test
