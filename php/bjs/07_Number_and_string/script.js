0c1e06cc5a03d06e36e779de275c41e410adfccf
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-01-01 15:26:48 +0300

    compleeted nums

--- php/bjs/07_Number_and_string/script.js
+++ php/bjs/07_Number_and_string/script.js
@@ -17,6 +17,34 @@ document.getElementById('btn_3').addEventListener('click', function () {
     inputWindow.value += '3';
 })
 
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+
 document.getElementById('btn_sum').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
     operation = 'sum';
